35b031f3db385f5ee3e73f33e1107e162d1fb10d
emraanadil <emraanadil07@gmail.com>
2022-08-03 22:30:18 +0530

    the commit to fall back to before deployment complete setup

--- DevClubLMS/settings.py
+++ DevClubLMS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r5($3_zye7)*g)*73j7av+g=unyuui2o9^c!z7x_s)5959&fjk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['emraanadil-lms.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['lms-emraanadil.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
